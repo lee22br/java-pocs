e382425fe133061cfc0328940e493935676247d7
leandro.iglezias <leeiglezias@gmail.com>
2025-09-14 22:48:13 -0300

    mavem Project - Jep507

---
+++ Java25/java25Features/src/main/java/Jep507.java
@@ -0,0 +1,14 @@
+public class Jep507 {
+
+
+    static void test(Object obj) {
+        if (obj instanceof int i) {
+            System.out.println("It's an int: " + i);
+        }
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
