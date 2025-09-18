2daff0287724db43f444ce806f7f0b7efe40f8e2
leandro.iglezias <leeiglezias@gmail.com>
2025-09-17 22:32:51 -0300

    mavem Project - JEP502

---
+++ Java25/java25Features/src/main/java/Jep502.java
@@ -0,0 +1,11 @@
+import java.lang.StableValue;
+
+public class Jep502 {
+    public static void main(String[] args) {
+        // Create a new unset StableValue
+        var greeting = StableValue.<String>of();
+
+        String message = greeting.orElseSet(() -> "Hello from StableValue!");
+        System.out.println(message);
+    }
+}
