914f7301c6eb75cd9068cd35b4b9731a5035522b
leandro.iglezias <leeiglezias@gmail.com>
2025-10-02 21:27:49 -0300

    Jep507

--- Java21/src/main/java/org/example/PrimitiveTypesPattern.java
+++ Java21/src/main/java/org/example/PrimitiveTypesPattern.java
@@ -1,5 +1,6 @@
-public class Jep507 {
+package org.example;
 
+public class PrimitiveTypesPattern {
 
     public static void main (String[] args){
         System.out.println("Test Primitive Types Patterns");
