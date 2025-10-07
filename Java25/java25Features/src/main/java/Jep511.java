b733bd883a415353a2c7a27a6870ae711cd0ff96
leandro.iglezias <leeiglezias@gmail.com>
2025-10-06 22:09:36 -0300

    mavem Project - Jep511 connection

--- Java25/java25Features/src/main/java/Jep511.java
+++ Java25/java25Features/src/main/java/Jep511.java
@@ -1,7 +1,27 @@
 
 import module java.base;
 
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Jep511 {
+
+   public static void main (String[] args) throws SQLException {
+       System.out.println("Test Module Import Declaration");
+
+       Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
+       System.out.println("Connection created: "+ (conn != null));
+
+       ConnectionPoolDataSource ds = new org.h2.jdbcx.JdbcDataSource();
+       DataSource dataSource = (DataSource) ds;
+       System.out.println("Datasource created: "+ dataSource);
+   }
+
+
+
 
     public static Map<Character, List<String>> groupByFirstLetter(String... values) {
         return Stream.of(values).collect(
