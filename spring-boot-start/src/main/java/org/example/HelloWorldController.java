9a4960602d9df23d920c1fb5f5e7e24799dd7dc7
leandro.iglezias <leeiglezias@gmail.com>
2025-11-01 14:04:45 -0300

    Hello World on Spring Web

--- spring-boot-start/src/main/java/org/example/HelloWorldController.java
+++ spring-boot-start/src/main/java/org/example/HelloWorldController.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,11 @@ public class HelloWorldController {
     @GetMapping("/cars")
     public String getCars() {
         return "All Cars";
+    }
+
+
+    @PostMapping("/car")
+    public String createCar() {
+        return "New Car";
     }
 }
