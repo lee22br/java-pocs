e6d78fa53214cb5789f9090b7147c63b5fc24725
leandro.iglezias <leeiglezias@gmail.com>
2025-11-07 16:49:49 -0300

    velocity

--- java-reports/src/main/java/org/example/App.java
+++ java-reports/src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.StringWriter;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Apache Velocity" );
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.init();
+
+        Template t = velocityEngine.getTemplate("index.vm");
+
+        VelocityContext context = new VelocityContext();
+        context.put("name", "World");
+
+        StringWriter writer = new StringWriter();
+        t.merge( context, writer );
+        System.out.println(t.process());
     }
 }
