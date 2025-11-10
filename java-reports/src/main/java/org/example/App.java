d2ae8373438f66b9297acb1f72e42aac81a0c92e
leandro.iglezias <leeiglezias@gmail.com>
2025-11-10 20:12:24 -0300

    velocity plus iText plus html2Pdf

--- java-reports/src/main/java/org/example/App.java
+++ java-reports/src/main/java/org/example/App.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.WriterProperties;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
+
+import java.io.FileOutputStream;
 import java.io.StringWriter;
 import java.util.Date;
 import java.util.Properties;
@@ -33,11 +39,25 @@ public class App
         context.put("numMsg", 42);
         context.put("date", new Date());
 
-        Template template = ve.getTemplate("velocity.vm");
+        Template template = ve.getTemplate("velocity.vm", "UTF-8");
 
         StringWriter writer = new StringWriter();
         template.merge(context, writer);
-
+        String htmlContent = writer.toString();
         System.out.println(writer.toString());
+
+        final String DEST = "relatorio.pdf";
+        try {
+            ConverterProperties properties = new ConverterProperties();
+            properties.setBaseUri("./src/main/resources/html/");
+            PdfWriter writerPdf = new PdfWriter(DEST, new WriterProperties().setFullCompressionMode(true));
+            FileOutputStream os = new FileOutputStream("relatorio.pdf");
+
+            HtmlConverter.convertToPdf(htmlContent, writerPdf, properties);
+            os.close();
+            System.out.println("PDF gerado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
