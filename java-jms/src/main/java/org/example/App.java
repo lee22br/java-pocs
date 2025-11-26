4482e2b2afc8382940483e2bb06a21f1ad0ee0f6
leandro.iglezias <leeiglezias@gmail.com>
2025-11-25 21:08:07 -0300

    Initial

---
+++ java-jms/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import java.util.Queue;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String argv[]) throws Exception {
+
+    }
+}
