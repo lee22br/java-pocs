f37cd3b7b126d4fefb7df6fa39643bf2807317da
leandro.iglezias <leeiglezias@gmail.com>
2025-12-02 22:02:33 -0300

    Initial

--- java-kafa/src/main/java/org/example/ConsumerKafka.java
+++ java-kafa/src/main/java/org/example/ConsumerKafka.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class ConsumerKafka {
+
+    public static void main(String[] args) {
+        // Configurações do Consumidor
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group01");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        try (Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Collections.singletonList("my-topic"));
+
+            System.out.println("Consumer initiated. Listen my-topic...");
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(500));
+                records.forEach(record -> {
+                    System.out.printf("Received -> Key: %s, Value: %s, Partition: %d, Offset: %d%n",
+                            record.key(), record.value(), record.partition(), record.offset());
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
