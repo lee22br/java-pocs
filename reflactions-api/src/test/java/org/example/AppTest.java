731631bf86cc3693054b7e530ab3cd26a9af7f0c
leandro.iglezias <leeiglezias@gmail.com>
2026-01-20 21:10:27 -0300

    POC Reflections API

---
+++ reflactions-api/src/test/java/org/example/AppTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testGetFields() {
+        Object person = new Person();
+        Field[] fields = person.getClass().getDeclaredFields();
+
+        List<String> actualFieldNames = getFieldNames(fields);
+
+        assertTrue(Arrays.asList("name", "age").containsAll(actualFieldNames));
+    }
+
+    private static List<String> getFieldNames(Field[] fields) {
+        List<String> fieldNames = new ArrayList<>();
+        for (Field field : fields)
+            fieldNames.add(field.getName());
+        return fieldNames;
+    }
+}
