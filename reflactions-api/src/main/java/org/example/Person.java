35b325882427261cdc8b0fd0e8d4e29d56603e0c
leandro.iglezias <leeiglezias@gmail.com>
2026-01-21 23:26:34 -0300

    POC Reflections API

--- reflactions-api/src/main/java/org/example/Person.java
+++ reflactions-api/src/main/java/org/example/Person.java
@@ -3,4 +3,9 @@ package org.example;
 public class Person {
     private String name;
     private int age;
+    private String words;
+
+    public String sayHello(){
+            return "Hello";
+    }
 }
--- reflactions-api/src/test/java/org/example/AppTest.java
+++ reflactions-api/src/test/java/org/example/AppTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -23,6 +26,17 @@ public class AppTest {
         List<String> actualFieldNames = getFieldNames(fields);
 
         assertTrue(Arrays.asList("name", "age").containsAll(actualFieldNames));
+    }
+
+    @Test
+    public void testInvoke() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<?> personClass = Class.forName("org.example.Person");
+        Person person = (Person) personClass.getConstructor().newInstance();
+        Method sayHelloMethod = personClass.getDeclaredMethod("sayHello");
+        String hello = (String) sayHelloMethod.invoke(person);
+
+        assertEquals("Hello",hello);
+
     }
 
     private static List<String> getFieldNames(Field[] fields) {
