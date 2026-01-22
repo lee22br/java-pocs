fb0ad8d169e037ee66b111ce7a5d146f527ae387
leandro.iglezias <leeiglezias@gmail.com>
2026-01-22 20:54:15 -0300

    POC Function interface

---
+++ functional-interfaces/src/test/java/org/example/AppTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AppTest 
+{
+    @Test
+    public void testAdd(){
+        MathOperation addition = (a,b) -> a + b;
+
+        assertEquals(2, addition.execute(1,1));
+    }
+
+    @Test
+    public void testMultiply(){
+        MathOperation multiplication = (a,b) -> a * b;
+
+        assertEquals(4, multiplication.execute(2,2));
+    }
+
+    @Test
+    public void test(){
+        MathOperation multiplication = Math::pow;
+
+        assertEquals(16, multiplication.execute(2,4));
+    }
+
+}
