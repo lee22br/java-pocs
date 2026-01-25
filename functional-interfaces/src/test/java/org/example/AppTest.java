10c7127c5011856d632ed742a9255c6531874378
leandro.iglezias <leeiglezias@gmail.com>
2026-01-24 23:15:31 -0300

    POC biFunctional

--- functional-interfaces/src/test/java/org/example/AppTest.java
+++ functional-interfaces/src/test/java/org/example/AppTest.java
@@ -2,7 +2,13 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.example.FunctionUtil.listCombiner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class AppTest 
@@ -26,6 +32,16 @@ public class AppTest
         MathOperation multiplication = Math::pow;
 
         assertEquals(16, multiplication.execute(2,4));
+    }
+
+    @Test void testBifunctional(){
+        List<String> list1 = Arrays.asList("a", "b", "c");
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+        List<String> list3 = Arrays.asList("a1", "b2", "c3");
+
+        List<String> result = listCombiner(list1, list2, (letter, number) -> letter + number);
+
+        assertTrue(result.containsAll(list3));
     }
 
 }
