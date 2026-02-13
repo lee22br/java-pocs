b77081722fe3e3defd3a3fbf374c06a27a5c9c1c
Leandro Iglezias <leandro.iglezias@lnaix.com.br>
2026-02-13 20:21:00 -0300

    fixed.

--- concurrency-2/src/main/java/org/example/App.java
+++ concurrency-2/src/main/java/org/example/App.java
@@ -6,24 +6,9 @@ package org.example;
  */
 public class App 
 {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-
-        Callable<Integer> task = () -> {
-            System.out.println("Processing thread: " + Thread.currentThread().getName());
-            Thread.sleep(1000);
-            return 42;
-        };
-
-        try {
-            Future<Integer> futuro = executor.submit(task);
-            
-            System.out.println("Do something...");
-            
-            System.out.println("Task result: " + futuro.get());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        } finally {
-            executor.shutdown();
-        }
+        
+    public static void main( String[] args )
+    {
+        
     }
 }
