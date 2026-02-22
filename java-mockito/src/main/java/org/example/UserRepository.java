78aa656fa03b93d43d3abb60af71b36d1066d7f0
leandro.iglezias <leeiglezias@gmail.com>
2026-02-22 11:05:44 -0300

    POC Mockito part 1

---
+++ java-mockito/src/main/java/org/example/UserRepository.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Optional;
+
+public class UserRepository {
+
+    public Optional<User> findById (Long id){
+        return Optional.empty();
+    }
+
+    public void save (User user){
+        //save
+    }
+}
