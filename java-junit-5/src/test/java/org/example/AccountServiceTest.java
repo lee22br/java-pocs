593326b0dded7bb5ef923b6c1c9e4f9dcd3b239f
leandro.iglezias <leeiglezias@gmail.com>
2026-03-01 20:15:49 -0300

    POC Java Junit5 part 2

--- java-junit-5/src/test/java/org/example/AccountServiceTest.java
+++ java-junit-5/src/test/java/org/example/AccountServiceTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,4 +37,22 @@ class AccountServiceTest {
             assertTrue(exception.getMessage().contains("Shortfall of 400.0"));
         }
     }
+
+    @ParameterizedTest
+    @DisplayName("Testing values source")
+    @ValueSource(strings = {"lee@gmail.com", "teste@outlook.com.br", "contato@rio.org.br"})
+    void palindromes(String candidate) {
+        assertTrue(User.isValidEmail(candidate));
+    }
+
+    @Test
+    @DisplayName("Testing Grouping")
+    void testVoidMethodError() {
+        User user = new User(1L, "Leandro", "lee@globo.com");
+        assertAll("user",
+                () -> assertEquals("Leandro", user.getName()),
+                () -> assertEquals("lee@globo.com", user.getEmail())
+        );
+    }
+
 }
